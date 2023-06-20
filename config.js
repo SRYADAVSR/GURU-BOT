d711a6ff2c65cf4c58b7855e53a47b9ae0b33d59
SRYADAVSR <137145449+SRYADAVSR@users.noreply.github.com>
2023-06-20 19:54:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['917047294917', '𝑅𝛥𝛭 𝐶𝛨𝛥𝑅𝛥𝛮', true],
+  ['917865054966', '𝑅𝛥𝛭 𝐶𝛨𝛥𝑅𝛥𝛮 2.00', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
+global.mods = ['917047294917','917865054966'] 
+global.prems = ['917047294917', '917865054966', '918927384393']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,9 +47,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
+global.botname = '𝑅𝛥𝛭 𝐶𝛨𝛥𝑅𝛥𝛮'
+global.premium = 'false'
+global.packname = 'S MUSIC PURULIA' 
 global.author = '@Asliguru' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
